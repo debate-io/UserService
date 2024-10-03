08b6a4df8f0162b190cd7e7cac1b5a65ff1ff295
VMenshchikov <vladmenss3@gmail.com>
2024-10-03 13:10:14 +0700

    Dev-12207: убрал лишнее

--- internal/domain/interfaces/user_repo/errors.go
+++
@@ -1,9 +0,0 @@
-package userepo
-
-import "github.com/ztrue/tracerr"
-
-var (
-	ErrUserAlreadyExist = tracerr.New("user already register")
-	ErrUserNotFound     = tracerr.New("user not found")
-	//role?
-)
